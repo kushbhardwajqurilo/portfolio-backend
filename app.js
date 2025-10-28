7bb743d6fb9f4a5e187fdba6bfce595d020b9e6e
kush <=>
2025-10-28 11:35:25 +0530

    cors update

--- app.js
+++ app.js
@@ -3,22 +3,32 @@ const cors = require("cors");
 const router = require("./src/router");
 
 const app = express();
+const allowedOrigins = [
+  "https://my-portfolio-two-gamma-22.vercel.app",
+  "https://4frnn03l-5173.inc1.devtunnels.ms",
+  "http://localhost:5173",
+];
 
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type , Authorization ,X-Requested-With"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(
-  cors({
-    origin: [
-      "https://my-portfolio-two-gamma-22.vercel.app",
-      "https://4frnn03l-5173.inc1.devtunnels.ms",
-      "http://localhost:5173",
-    ],
-    methods: ["GET", "POST", "OPTIONS"],
-    credentials: true,
-  })
-);
-
 app.use("/visitor", router);
 
 module.exports = app;
